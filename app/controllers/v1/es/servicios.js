8207a60d31826df14c83d65bd5cf8da75f4c675a
FBV <fbordallo2@gmail.com>
2015-12-22 20:30:59 +0100

    [v1][es][services] - Extended documentation. Included documentation for goAndHide() method.

--- app/controllers/v1/es/servicios.js
+++ app/controllers/v1/es/servicios.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+
+  actions:{
+    focusHide: function(id, animation, speed){
+      id = '#' + id;
+      this.Animate.goAndHide(id, animation, speed);
+    }
+  }
+
 });
